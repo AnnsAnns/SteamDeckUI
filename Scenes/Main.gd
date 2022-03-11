d11d70714081ba07d27c518ab6d6206b3e27ea17
tumGER <git@tomger.eu>
2022-03-11 16:33:50 +0100

    B pressing to home

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -11,6 +11,10 @@ func _ready():
 	$FileDialog.show() # Replace with function body.
 
 
+func _process(delta):
+	if Input.is_action_just_pressed("b_test"):
+		$FileDialog.current_path = "/home/"
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
