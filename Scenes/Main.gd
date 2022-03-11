a7debf957d2eba3a0c2c8f3b20f0217ebc4a5b34
tumGER <git@tomger.eu>
2022-03-11 16:25:54 +0100

    AAA

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$FileDialog.popup() # Replace with function body.
+	$FileDialog.show() # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
